9ef80a6fa34433de2576d43be03059d7fea9a39f
Marco Dalla Ba <m.dallaba@reply.it>
2020-10-17 11:54:11 +0200

    Implemented moshi Created PokeService

---
+++ app/src/main/java/com/marcodallaba/pokeapp/api/PokeService.kt
@@ -0,0 +1,43 @@
+package com.marcodallaba.pokeapp.api
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PokeService {
+
+    @GET("pokemon")
+    suspend fun searchPokemon(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    )
+
+    companion object {
+        private const val BASE_URL = "https://pokeapi.co/api/v2/"
+
+        fun create(): PokeService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .build()
+                .create(PokeService::class.java)
+        }
+    }
+}
